2df311863516480f28e21d416168d26567e58c79
RahulGajare <42006160+RahulGajare@users.noreply.github.com>
2021-09-01 14:40:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/ui/PokemonListScreen.kt
@@ -0,0 +1,9 @@
+package com.example.pokemon.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PokemonListScreen()
+{
+
+}
---
+++ app/src/main/java/com/example/pokemon/ui/Pokemon_DetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.pokemon.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Pokemon_DetailScreen()
+{
+
+}
